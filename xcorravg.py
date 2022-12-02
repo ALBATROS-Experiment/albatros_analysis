9254bfb9d00c75c4d2710dbe51f5f52fd7a3aa98
Mohan <mohan.agr97@gmail.com>
2022-12-02 11:31:45 -0500

    working 2 ant xcorr examples

--- xcorravg.py
+++ xcorravg.py
@@ -10,11 +10,15 @@ def get_avg_fast(path1, path2, init_t, end_t, delay, acclen, nchunks, chanstart=
     
     idxstart1, fileidx1, files1 = butils.get_init_info(init_t, end_t, path1)
     idxstart2, fileidx2, files2 = butils.get_init_info(init_t, end_t, path2)
-
+    idxstart1=2502441
+    idxstart2=1647949
+    print(idxstart1,idxstart2, "IDXSTARTS")
     if(delay>0):
         idxstart2+=delay
     else:
         idxstart1+=np.abs(delay)
+
+    print(idxstart1,idxstart2)
 
     # print("Starting at: ",idxstart, "in filenum: ",fileidx)
     # print(files[fileidx])
@@ -28,7 +32,8 @@ def get_avg_fast(path1, path2, init_t, end_t, delay, acclen, nchunks, chanstart=
     st=time.time()
     for i, (chunk1,chunk2) in enumerate(zip(ant1,ant2)):
         t1=time.time()
-        pol00[i,:] = cr.avg_xcorr_4bit_2ant(chunk1['pol0'], chunk2['pol0'],chunk1['specnums'],chunk2['specnums'],m1+i*acclen,m2+i*acclen)
+        # pol00[i,:] = cr.avg_xcorr_4bit_2ant(chunk1['pol0'], chunk2['pol0'],chunk1['specnums'],chunk2['specnums'],m1+i*acclen,m2+i*acclen)
+        pol00[i,:] = cr.avg_xcorr_4bit_2ant(chunk2['pol0'], chunk1['pol0'],chunk2['specnums'],chunk1['specnums'],m2+i*acclen,m1+i*acclen)
         t2=time.time()
         print("time taken for one loop", t2-t1)
         j=ant1.spec_num_start
@@ -41,30 +46,41 @@ def get_avg_fast(path1, path2, init_t, end_t, delay, acclen, nchunks, chanstart=
 if __name__=="__main__":
     path1='/project/s/sievers/albatros/uapishka/baseband/snap1'
     path2='/project/s/sievers/albatros/uapishka/baseband/snap3'
-    init_t = 1627441542 #1627439234
+    init_t = 1627439234#1627441379 #1627441542 #1627439234
     acclen=10000
     t_acclen = acclen*4096/250e6
-    delay=-963933#33005
-    nchunks=1000
+    delay=-34060 #-50110 #-963933
+    nchunks=2947 #1959
     end_t = int(init_t + nchunks*t_acclen)
     pol00,channels=get_avg_fast(path1, path2, init_t, end_t, delay, acclen, nchunks, chanstart=35, chanend=50)
 
     from matplotlib import pyplot as plt
     plt.figure(figsize=(5,10), dpi=200)
     plt.suptitle(f'Delay {delay}, acclen {acclen}')
-    myext = np.array([np.min(channels)*125/2048,np.max(channels)*125/2048, pol00.shape[0]*t_acclen/60, 0])
+    myext = np.array([np.min(channels)*125/2048,np.max(channels)*125/2048, pol00.shape[0]*t_acclen, 0])
     plt.subplot(211)
     plt.imshow(np.log10(np.abs(pol00)), aspect='auto', extent=myext,interpolation='none')
     plt.title('pol01 magnitude')
     plt.colorbar()
 
+    ph = np.angle(pol00)
     plt.subplot(212)
-    plt.imshow(np.arctan2(pol00.imag,pol00.real), aspect='auto', extent=myext, cmap='RdBu',interpolation='none')
+    plt.imshow(ph, aspect='auto', extent=myext, cmap='RdBu',interpolation='none')
     plt.title('pol01 phase')
     plt.colorbar()
 
-    plt.savefig('/scratch/s/sievers/mohanagr/mytempfig.png')
-    print('/scratch/s/sievers/mohanagr/mytempfig.png')
+    # plt.subplot(223)
+    # plt.plot(ph[:,2])
+    # plt.title('wrapped phase for one channel')
+    
+    # plt.subplot(224)
+    # plt.plot(np.unwrap(ph[:,2]))
+    # plt.title('unwrapped phase for one channel')
+    
+
+
+    plt.savefig('/scratch/s/sievers/mohanagr/mytempfig3.png')
+    print('/scratch/s/sievers/mohanagr/mytempfig3.png')
 
 
 
