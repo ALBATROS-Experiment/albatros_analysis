dea99f6faf28dd0de89c13144678b92f70502751
CherieDay <denzata@yahoo.com>
2023-05-19 14:17:32 -0400

    moved tot_minutes and tstop definitions to after pol arrays are defined to get the correct plot extent

--- quick_spectra.py
+++ quick_spectra.py
@@ -61,13 +61,9 @@ if __name__ == "__main__":
     cstart = int(np.floor(fmin/(250/4096)))
     cend = int(np.floor(fmax/(250/4096)))
     
-    tot_minutes = int(np.ceil(acctime * pol00.shape[0]/60))
     tstart = 0 #for myext below
-    tstop = tstart + tot_minutes
     if args.tslice is not None:
         tstart = int(args.tslice.start)
-        tstop  = int(args.tslice.stop)
-
     
     if args.tslice:
         #convert tslice in minutes to samps
@@ -83,6 +79,11 @@ if __name__ == "__main__":
         pol01r = pol01r[1:,cstart:cend]
         pol01i = pol01i[1:,cstart:cend]
 
+    tot_minutes = int(np.ceil(acctime * pol00.shape[0]/60))
+    tstop = tstart + tot_minutes
+    if args.tslice is not None:
+        tstop  = int(args.tslice.stop)
+    
     # Add real and image for pol01
     pol01 = pol01r + 1J*pol01i
     
