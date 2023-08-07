c628c7319d8e248ac3ca192b9473bcad77e6e68c
Mohan <mohan.agr97@gmail.com>
2023-08-07 17:18:51 -0400

    add get_file_from_timestamp

--- utils/baseband_utils.py
+++ utils/baseband_utils.py
@@ -1,32 +1,105 @@
 import numpy as np
-import os, glob
+import os, warnings
 from matplotlib import pyplot as plt
 
 
+def get_file_from_timestamp(ts, parent_dir, search_type, force_ts=False):
+    """Given a timestamp, return the file inside of which that timestamp lies.
+    The function works with both baseband and direct spectra files.
+
+    Parameters
+    ----------
+    ts : int or str
+        The timestamp you're interested in. (ctime)
+    parent_dir : str
+        The directory which contains 5 digit folders.
+    search_type : 'd' or 'f'
+        'd' (directory) if you are using this function for direct spectra, 'f' (file) for baseband.
+    force_ts : bool, default False
+        Return the next available file if a file containing ts is not found.
+
+    Returns
+    -------
+    str
+        Absolute path to the file which contains your timestamp.
+
+    Raises
+    ------
+    NotFoundError
+        If there is no matching file, the user is required to start their integration
+        from the next available timestamp that's helpfully suggested.
+    """
+
+    if search_type == "f":
+        stamp = ts[:6]
+    elif search_type == "d":
+        stamp = ts[:5]
+    else:
+        raise RuntimeError("invalid search type passed. Can only be 'f' or 'd' ")
+
+    op = subprocess.run(
+        ["find", parent_dir, "-type", search_type, "-name", f"{stamp}*"],
+        capture_output=True,
+    ).stdout.decode("utf-8")
+    # print(op)
+    files = op.split()
+    files.sort()  # files need to be in the same order as the timestamps, so we can simply pick the correct file later
+    tstamps = np.asarray([int(s.split("/")[-1].split(".")[0]) for s in files])
+    tstamps.sort()  # could remove this.
+    if search_type == "d":
+        delta = 3600  # direct spectra files are time-limited files. no need to run a median.
+    else:
+        delta = np.median(
+            np.diff(tstamps)
+        )  # find the time period. assumption: usually there will be several files in an hour.
+    flip = np.where(np.diff(tstamps > ts) != 0)[0]
+    print(delta, flip)
+    # plt.plot(tstamps>ts)
+    if ts - tstamps[flip] <= delta:
+        return files[flip]
+    else:
+        if force_ts:
+            warnings.warn(
+                f"Returning a file whose start is {tstamps[flip + 1]}, {tstamps[flip + 1] -  ts} seconds away from your requested timestamp"
+            )
+            return files[flip + 1]
+        else:
+            raise FileNotFoundError(
+                f"No file match for requested timestamp. Perhaps there was a data acquisition gap.
+                Next available file is {files[flip+1]}. Use that timestamp or use force_ts = True."
+            )
+
+    print(
+        ts - tstamps[flip]
+    )  # should be less than equal to delta, then our tstamp lies in a file.
+    # otherwise there's no such file with that timestamp. tell user to start from the next future timestamp
+    # force_ts = True  may be?
+
+
 def get_init_info(init_t, end_t, parent_dir):
-    """Get relevant indices from timestamps. 
-    
+    """Get relevant indices from timestamps.
+
     Returns the index of file in a folder and
     the index of the spectra in that file corresponding to init_timestamp
 
     Parameters
     ----------
     init_t: int or float
-        Start timestamp of time we are interested in. (in what units??)
+        Start timestamp of time we are interested in. (ctime)
     end_t: int or float
-        End of time window we are interested in. (in what units??)
+        End of time window we are interested in. (ctime)
     parent_dir: str
-        The directory we search in. 
+        The directory we search in.
 
     Returns
     -------
     idxstart: int
-        The index of the starting row of interest to us within a 
-        baseband data file. 
+        The index of the starting row of interest to us within a
+        baseband data file.
     fileidx: int
-        The index of the file in the sorted directory. 
+        The index of the file in the sorted directory.
     files: list of str
-        Sorted list of path strings to all files in 'parent_dir'.  
+        Sorted list of path strings to all files in 'parent_dir'.
     """
     # create a big list of files from 5 digit subdirs. we might not need all of them, but I don't want to write regex.
     # This may be faster, and I don't care about storing a few 100 more strings than I need to.
@@ -41,7 +114,7 @@ def get_init_info(init_t, end_t, parent_dir):
         files.append(glob.glob(path + "/*"))
     files.sort()
     speclen = 4096  # length of each spectra
-    fs = 250e6      # Hz
+    fs = 250e6  # Hz
     dt_spec = speclen / fs  # time taken to read one spectra (frame)
 
     # find which file to read first
@@ -62,14 +135,14 @@ def get_init_info(init_t, end_t, parent_dir):
     # check that starting index indeed corresponds to init_t
     print("Fileidx:", fileidx)
     print("CHECK", init_t, idxstart * dt_spec + filetstamps[fileidx])
-    print("CHECK", filetstamps[fileidx], files[fileidx]) # What does this check?
+    print("CHECK", filetstamps[fileidx], files[fileidx])  # What does this check?
 
     return idxstart, fileidx, files
 
 
 def get_plot_lims(pol, acclen):
     """Get limits for display settings for pretty plots!
-    
+
     Parameters
     ----------
     pol: np.ndarray
@@ -84,7 +157,7 @@ def get_plot_lims(pol, acclen):
     vmin: float
         Two standard deviations below mean.
     vmax: float
-        Two standard deviations above mean. 
+        Two standard deviations above mean.
     """
     # numpy percentile method ignores mask and may generate garbage with 0s (missing specs).
     # Pivot to using mean if acclen too small.
@@ -122,35 +195,35 @@ def plot_4bit(
 ):
     """Waterfall plotting routine for 4-bit spectrum integrated data.
 
-    Plots and saves figure. 
-    
+    Plots and saves figure.
+
     Parameters
     ----------
-    pol00: np.ndarray 
-        Channelized autocorr data from pol0. 
+    pol00: np.ndarray
+        Channelized autocorr data from pol0.
     pol11: np.ndarray
-        Channelized autocorr data from pol1. 
+        Channelized autocorr data from pol1.
     pol01: np.ndarray
-        Channelized x-corr data (pol0 x pol1). 
+        Channelized x-corr data (pol0 x pol1).
     channels: array like
-        Indices of channels of interest. 
+        Indices of channels of interest.
     acclen: int
         ??
     time_start: int or float
         ??In what format??
     vmin: float
-        Plotting parameter. Two standard deviations below mean. Used 
-        to set colorbar scale. 
+        Plotting parameter. Two standard deviations below mean. Used
+        to set colorbar scale.
     vmax: float
-        Plotting parameter. Two standard deviations above mean. Used 
-        to set colorbar scale. 
+        Plotting parameter. Two standard deviations above mean. Used
+        to set colorbar scale.
     opath: str
-        Ouput path. Path to image of figure to be saved. 
+        Ouput path. Path to image of figure to be saved.
     minutes: bool
-        Whether to display time in minutes. Defaults to False, 
-        displaying seconds. 
+        Whether to display time in minutes. Defaults to False,
+        displaying seconds.
     logplot: bool
-        Defaults to True. Logarithmic scale on y-axis. 
+        Defaults to True. Logarithmic scale on y-axis.
     """
     freq = channels * 125 / 2048  # MHz
     pol00_med = np.ma.median(pol00, axis=0)
@@ -162,8 +235,12 @@ def plot_4bit(
     pol00_min = np.ma.min(pol00, axis=0)
     pol11_min = np.ma.min(pol11, axis=0)
     if (vmin is None) and (vmax is None):
-        med, vmin, vmax = get_plot_lims(pol00, acclen) # use of acclen in get_plot_lims is depricated
-        med2, vmin2, vmax2 = get_plot_lims(pol11, acclen) # use of acclen in get_plot_lims is depricated
+        med, vmin, vmax = get_plot_lims(
+            pol00, acclen
+        )  # use of acclen in get_plot_lims is depricated
+        med2, vmin2, vmax2 = get_plot_lims(
+            pol11, acclen
+        )  # use of acclen in get_plot_lims is depricated
     else:
         print("SETTING VMIN AND VMAX")
         vmin = 10**vmin
@@ -192,7 +269,7 @@ def plot_4bit(
     plt.figure(figsize=(18, 10), dpi=200)
     t_acclen = acclen * 2048 / 125e6  # seconds
     t_end = pol01.shape[0] * t_acclen
-    tag = "Seconds" # Warning: confusing variable name, tag is used in SNAPfiletools in very different way/meaning
+    tag = "Seconds"  # Warning: confusing variable name, tag is used in SNAPfiletools in very different way/meaning
     if minutes:
         t_end = t_end / 60
         tag = "Minutes"
@@ -254,32 +331,32 @@ def plot_4bit(
     plt.title("pol01 phase")
     plt.colorbar()
     plt.savefig(opath)
-    return 
+    return
 
 
 def plot_1bit(pol01, channels, acclen, time_start, opath, minutes=False, logplot=False):
     """Waterfall plotting routine for 1-bit spectrum integrated data.
 
-    Plots and saves figure. 
-    
+    Plots and saves figure.
+
     Parameters
     ----------
     pol01: np.ndarray
-        Channelized x-corr data (pol0 x pol1). 
+        Channelized x-corr data (pol0 x pol1).
     channels: array like
-        Indices of channels of interest. 
+        Indices of channels of interest.
     acclen: int
         ??
     time_start: ??
         ??In what format; is this relative time or absolute timestamp?
     opath: str
-        Ouput path. Path to image of figure to be saved. 
+        Ouput path. Path to image of figure to be saved.
     minutes: bool
-        Whether to display time in minutes. Defaults to False, 
-        displaying seconds. 
+        Whether to display time in minutes. Defaults to False,
+        displaying seconds.
     logplot: bool
-        *Depricated.* 
-        Defaults to True. Logarithmic scale on y-axis. 
+        *Depricated.*
+        Defaults to True. Logarithmic scale on y-axis.
     """
     fig, ax = plt.subplots(1, 2)
     fig.set_size_inches(10, 4)
@@ -305,4 +382,4 @@ def plot_1bit(pol01, channels, acclen, time_start, opath, minutes=False, logplot
     plt.colorbar(img1, ax=ax[0])
     plt.colorbar(img2, ax=ax[1])
     plt.savefig(opath)
-    return 
+    return
