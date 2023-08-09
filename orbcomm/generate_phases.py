1f982a74e0398f80feebee85c1e228fccf418a97
Mohan <mohan.agr97@gmail.com>
2023-08-09 11:50:57 -0400

    minor changes

--- orbcomm/generate_phases.py
+++ orbcomm/generate_phases.py
@@ -1,5 +1,6 @@
 from utils.orbcomm_utils import get_risen_sats, find_pulses, gauss_smooth
 
+
 def filter_single_sat(risen_sat_count):
     """Get epoch start and end indices corresponding to periods during which only one satellite is risen
 
@@ -9,16 +10,27 @@ def filter_single_sat(risen_sat_count):
         count of risen satellites at each epoch
     """
 
-    ss_epochs = find_pulses(risen_sat_count, cond="==", thresh=1) #signal is e.g. _______|-----|____|---|__ ON when only 1 sat
+    ss_epochs = find_pulses(
+        risen_sat_count, cond="==", thresh=1
+    )  # signal is e.g. _______|-----|____|---|__ ON when only 1 sat
+
 
 def find_single_sat_transits(spectra, acctime=None):
-
     nspec, nchan = spectra.shape
-    spectra = 10*np.log10(spectra/np.median(spectra)) # convert to SNR in dB. median inside log same as median outside log since log is strictly monotonously increasing.
-    spectra = np.apply_along_axis(smooth,0,spectra)
+    # convert to SNR in dB. median inside log same as median outside log since log is strictly monotonously increasing.
+    # gauss smooth along time after appending zeros
+    spectra = np.apply_along_axis(
+        gauss_smooth,
+        0,
+        np.vstack(
+            [
+                10 * np.log10(spectra / np.median(spectra)),
+                np.zeros((nspec, nchan), dtype=spectra.dtype),
+            ]
+        ),
+    )[:nspec, :]
     transits = {}
     for chan in range(0, nchan):
-        transits[chan] = find_pulses(spectra[:,chan], cond=">", thresh=5)
-
-
-
+        transits[chan] = find_pulses(spectra[:, chan], cond=">", thresh=5)
+    
+    return transits
