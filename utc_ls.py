649700c660d28e245b65eb2d6898ac3b1790010f
tristanmenard <tristan.menard@outlook.com>
2023-09-01 13:42:20 -0400

    added timezone selection functionality

--- utc_ls.py
+++ utc_ls.py
@@ -1,15 +1,27 @@
-import datetime, tabulate, sys, os
+import datetime, tabulate, os, pytz
 import numpy as np
+import argparse
 
-# Just a stupid convenience script for listing ctime-stamped
-# directories and printing out equivalent UTC in human-friendly
-# format.  Usage is
-#    python utc_ls.py /path/to/directories/*
+def get_timezone(timezone):
+    """
+    Transform given timezone name (e.g. "US/Eastern") into a pytz timezone object.
+    Recognizes names of common observation sites (Marion, MARS, Uapishka).
+    Return None if given timezone name is not recognized.
+    """
+    if timezone:
+        if timezone in pytz.all_timezones:
+            return pytz.timezone(timezone)
+        if timezone.lower() == "marion":
+            return pytz.timezone("Africa/Nairobi")
+        if timezone.lower() == "mars":
+            return pytz.timezone("America/Winnipeg")
+        if timezone.lower() == "uapishka":
+            return pytz.timezone("America/Toronto")
+
+    return None
 
 
-
-#============================================================  
-def files_to_human_time(parent_dir):
+def files_to_human_time(parent_dir, timezone=None):
     '''
     Author: Joelle, May 2022
 
@@ -20,13 +32,27 @@ def files_to_human_time(parent_dir):
 
     files = np.sort(np.array(os.listdir(parent_dir),dtype=int))
     utc_dates = [datetime.datetime.utcfromtimestamp(i).strftime("%m-%d-%Y  %H:%M:%S")for i in files]
-    local_dates = [datetime.datetime.fromtimestamp(i).strftime("%m-%d-%Y  %H:%M:%S")for i in files]
-    
+    local_dates = [datetime.datetime.fromtimestamp(i, timezone).strftime("%m-%d-%Y  %H:%M:%S")for i in files]
+
     d = np.vstack((files,utc_dates,local_dates)).T
-    print(tabulate.tabulate(d,headers=["Timestamp","UTC date","Local timezone date"],stralign="center"))
+    if timezone:
+        return tabulate.tabulate(d,headers=["Timestamp","UTC date","Local date ({})".format(str(timezone))],stralign="center")
+    else:
+        return tabulate.tabulate(d,headers=["Timestamp","UTC date","My system date"],stralign="center")
 
 
 if __name__ == '__main__':
+    # Just a stupid convenience script for listing ctime-stamped
+    # directories and printing out equivalent UTC in human-friendly
+    # format.  
+    # Example usage: python utc_ls.py ~/auto_cross_data/16528/ -tz MARS
 
-    args = sys.argv[1]
-    files_to_human_time(args)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("path", type=str, nargs="+", help="Path to directory that contains Unix timestamp subdirectories. Ex: 16528/ which contains 1652800704/")
+    parser.add_argument("-tz", "--timezone", type=str, default=None, help="Local timezone of the telescope (must be recognized by pytz). Ex: US/Eastern. Default is None.")
+    args = parser.parse_args()
+
+    timezone = get_timezone(args.timezone)
+
+    for path in args.path:
+        print(files_to_human_time(path, timezone))
