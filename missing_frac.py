65997d3e1c975711b97f8d736859a9ea6d084b57
Mohan <mohan.agr97@gmail.com>
2024-03-25 12:52:48 -0400

    add new way of calculating missing frac

--- missing_frac.py
+++ missing_frac.py
@@ -8,9 +8,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def get_missing_frac(fname):
+    '''
+        E.g. say we write 100 spectra per file (could be discontinuous if stuff goes missing); baseband files are size limited.
+        old way :   file says I should have 130 spectra (last specnum - first specnum), but 30 of them are missing (discontinuties)
+                    so missing frac is 30/130
+        new way :   I know we always write 100 spectra per file. file says I should have 130 spectra (apparent total)
+                    which means 30 of them went missing. so missing frac is 30/100.
+        Essentially, new way will be slightly higher than the old way.
+    '''
     obj=bdc.Baseband(fname)
-    missing_frac = np.sum(obj.missing_num)/(obj.spec_num[-1]-obj.spec_num[0]+obj.spectra_per_packet)
-    return missing_frac
+    missing_frac_old = np.sum(obj.missing_num)/(obj.spec_num[-1]-obj.spec_num[0]+obj.spectra_per_packet)
+    missing_frac_new = (obj.spec_num[-1]-obj.spec_num[0]+obj.spectra_per_packet)/(obj.num_packets*obj.spectra_per_packet) - 1
+    return missing_frac_old, missing_frac_new
 
 
 if __name__=='__main__':
@@ -24,17 +33,18 @@ if __name__=='__main__':
     files = glob(os.path.abspath(args.dirpath)+'/*.raw')
     # print(files)
     files.sort()
-    fracs = np.zeros(len(files))
+    fracs = np.zeros((len(files),2))
     try:
         for i,file in enumerate(files):
             tag=file.split('/')[-1]
             fracs[i] = get_missing_frac(file)
-            print(f"File {tag}, missing frac = {fracs[i]*100:5.3f}%")
+            print(f"File {tag}, missing frac old = {fracs[i][0]*100:5.3f}% | missing frac new = {fracs[i][1]*100:5.3f}%")
         # print("Missing fracs are:", fracs)
     except KeyboardInterrupt:
         print("exiting...")
     finally:
-        plt.plot(fracs[:i])
+        plt.plot(fracs[:i],label=['old','new'])
+        plt.legend()
         plt.show()
 
 
