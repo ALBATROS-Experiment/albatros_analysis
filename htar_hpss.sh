b084b3e99926eb97cd18c2407671a6e9af67e9e0
Mohan Agrawal <mohan.agr97@gmail.com>
2024-05-14 19:59:05 -0400

    robustify backup script against bad dirs/bad names/bad inputs

--- htar_hpss.sh
+++ htar_hpss.sh
@@ -1,11 +1,3 @@
-#!/bin/bash -l
-#SBATCH -t 17:00:00
-#SBATCH -p archivelong 
-#SBATCH -N 1
-#SBATCH --mail-type=END,FAIL
-#SBATCH -o /project/s/sievers/albatros/uapishka/202308/baseband/snap8/backup_uapishka_aug2023_quarry_%j.txt
-
-# NO FORWARD SLASHES IN THE PATHS 
 trap "echo 'Job script not completed';exit 129" TERM INT
 if [ $# -ne 2 ]; then
     echo "Usage: $0 <Source dir (scratch)> <Target dir (archive)>"
@@ -13,30 +5,32 @@ if [ $# -ne 2 ]; then
 fi
 echo "Backing up files in $1 to $2"
 shopt -s nullglob
-SRC="$1"
-DEST="$2"
+SRC=$(realpath "$1")
+DEST=$(realpath "$2") #remove trailing slashes and relative paths
 maxfiles=10 #number of files in one tarball
-cd $SRC
-DIRS=(*)
+DIRS=( $(find "$SRC" -mindepth 1 -type d -regextype egrep -regex ".*/[0-9]{5}") ) #find valid 5-digit dirs : absolute paths
 
 #create 5 digit dirs in archive
 for dir in ${DIRS[@]}
 do
-	echo "Checking for $dir"
+    stamp=${dir##*/} #get 5 digit sub-dir stamp by removing longest substring ending with /
+    echo "Checking 5-digit tstamp $stamp of $dir"
 	hsi ls "$DEST/$dir" &> /dev/null
+    #ls "$DEST/$stamp" &> /dev/null
 	status=$?
 	# echo $status
 	if [[ $status == 0 ]]
 	then
-		echo "dir $dir exists at destination"
+		echo "dir $stamp exists at destination"
 	else
 		hsi mkdir "$DEST/$dir" &> /dev/null
-		echo "Created dir $dir at destination"
+        #mkdir "$DEST/$stamp" &> /dev/null
+		echo "Created dir $stamp at destination"
 	fi
 done
 
 #loop over each 5 digit dir
-for d in $SRC/* #be careful, another / and stamp will fail (below)
+for d in ${DIRS[@]} #be careful, another / and stamp will fail (below)
 do
         stamp=${d##*/} #get 5 digit sub-dir stamp by removing longest substring ending with /
 	cd $d
@@ -44,13 +38,15 @@ do
 	files=($files)
 	numfiles=${#files[@]}
 	echo "num of files in $d is" $numfiles
-	#echo ${files[@]}
+	# echo ${files[@]}
     	#loop over blocks of size maxfiles in each 5-digit dir
 	for ((i=0;i<${#files[@]};i+=maxfiles))
 	do
 		tstamp=`echo ${files[$i]} | grep -o "[0-9]\{10\}"`
 		fpath=$DEST/$stamp/$tstamp.tar
-		hsi ls "$fpath" &> /dev/null
+        #echo $fpath
+	        hsi ls "$fpath" &> /dev/null
+        #ls "$fpath" &> /dev/null
 		status=$?
 		if [ $status == 0 ]; then  
 		    echo "File $fpath already exists. Continuing..."
@@ -59,6 +55,7 @@ do
 		# echo "file tstamp is" $tstamp
 		# echo ${files[@]:$i:$maxfiles} # get relevant files
 		htar -Humask=0137 -cpf "$fpath" -Hcrc -Hverify=1 "${files[@]:$i:$maxfiles}"
+                #tar -cpf "$fpath" "${files[@]:$i:$maxfiles}"
 		status=$?
 		if [ ! $status == 0 ]; then
 		    echo 'HTAR returned non-zero code. while trying to tar files: ' "${files[@]:$i:$maxfiles}"
