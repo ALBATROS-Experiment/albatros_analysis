3f566bdd242ab4921fb9287a16698666ed7a0318
Mohan <mohan.agr97@gmail.com>
2024-09-20 18:14:46 -0400

    added tests for checking specnum wrap handling

--- src/correlations/tests/generate_test_baseband.py
+++ src/correlations/tests/generate_test_baseband.py
@@ -1,6 +1,9 @@
 import numpy as np
 import time
+import os
 
+file_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(file_dir, "data")
 
 def get_channels_from_str(chan, nbits):
     """This function is copied from albatros_daq_utils, used in dump_baseband and config_fpga. Originally writenn by Jon(?). The channels returned by this function essentially correspond to the channel for each byte in the spectrum.
@@ -98,25 +101,79 @@ def write_header(file_object, chans, spec_per_packet, bytes_per_packet, bits):
     latlon.tofile(file_object)
 
 
-def dump_raw_data():
-    spec_per_packet = 1
+def dump_data(start_specnum, spec_per_packet, num_packets, missing_mask, filename, bits=4):
+    """Dump baseband with all spectra bytes set to 1. Allows custom-crafting of missing packets.
+    Parameters
+    ----------
+    start_specnum : int64
+        Starting specnum
+    num_packets : int
+        Total number of packets
+    missing_mask : list
+        Array of 0's and 1's. numpy.ma format: 1 if missing
+        Choose which packets to save and which to "drop" in order to simulate missing packets
+    spec_per_packet : int64
+        Spectra per packet
+    filename : str
+        Name of baseband file to save to data directory
+    bits : int, optional
+        Bit depth of the fake baseband data, by default 4.
+        Unpacking codes will use this parameter when decompressing the baseband 
+        into floating point complex numbers.
+    """
     bits = 4
-    chans = get_channels_from_str("452:732", bits)
+    chans = get_channels_from_str("452:454", bits)
     bytes_per_spectra = len(chans)
     bytes_per_packet = bytes_per_spectra * spec_per_packet + 4
-    spectra = np.ones(bytes_per_spectra, dtype="B")
-    specnum = np.asarray([123456789], dtype=">I")
-    packet = np.array(
-        (specnum, spectra),
-        dtype=[("spec_num", ">I"), ("spectra", "%dB" % (len(spectra)))],
-    )
-    print(packet)
+    print("bytes per packet", bytes_per_packet)
+    # spectra = np.ones(bytes_per_packet-4, dtype="B")
+    if isinstance(start_specnum,np.ndarray):
+        specnums = start_specnum.astype(">I")
+    else:
+        specnums = (start_specnum + np.arange(num_packets,dtype='int64')*spec_per_packet).astype(">I") #big endian uint32
+    print(specnums)
+    print(missing_mask)
     with open(
-        "/home/mohan/Projects/albatros_analysis/correlations/tests/header.raw", "w"
+        os.path.join(data_dir, filename), "wb"
     ) as f:
         write_header(f, chans, spec_per_packet, bytes_per_packet, bits)
-        packet.tofile(f)
-
+        for pnum in range(num_packets):
+            if not missing_mask[pnum]:
+                # chan 0 = 0+1j chan 1 = 1+0j
+                # pol0 = pol1
+                spectrum=np.ones(bytes_per_spectra,dtype='u1') # 
+                spectrum[2:]=spectrum[2:]<<4
+                packet = np.asarray(
+                    (specnums[pnum], np.tile(spectrum, spec_per_packet)),
+                    dtype=[("spec_num", ">I"), ("spectra", "%dB" % (bytes_per_packet-4))],
+                )
+                packet.tofile(f)
+                print(packet)
 
 if __name__ == "__main__":
-    dump_raw_data()
+    # chans = get_channels_from_str("452:454", 4)
+    # bytes_per_spectra = len(chans)
+    # num_packets=1
+    # spec_per_packet=5
+    # bytes_per_packet=bytes_per_spectra*spec_per_packet+4
+    # print(bytes_per_packet)
+    # with open(
+    #     os.path.join(data_dir, "test_baseband.raw"), "wb"
+    # ) as f:
+
+    #     write_header(f, chans, spec_per_packet, bytes_per_packet, 4)
+    #     packet = np.asarray(
+    #         (500, np.ones(20,dtype='u1')),
+    #         dtype=[("spec_num", ">I"), ("spectra", "%dB" % (20))],
+    #     )
+    #     packet.tofile(f)
+    #     print(packet)
+    dump_data(2**32-10,5,7,[0,0,1,0,0,0,0],"specnum_wrap1.raw") #start at 4294967286
+    # (4294967286, [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    # (4294967291, [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    # (5,          [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    # (10,         [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    # (15,         [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    # (20,         [ 1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16,  1,  1, 16, 16])
+    dump_data(25,5,3,[0,0,0],"specnum_wrap2.raw") #start at 25 continuing from previous file
+    dump_data(np.asarray([40,5,10]),5,3,[0,0,0],"specnum_wrap3.raw") #introduce another wrap
