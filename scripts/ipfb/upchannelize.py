2f8109d8266f6ba8d89e79fa87841150a409ac08
Mohan Agrawal <mohan.agr97@gmail.com>
2024-11-01 13:31:55 -0400

    upchannelization template

---
+++ scripts/ipfb/upchannelize.py
@@ -0,0 +1,25 @@
+import os
+import sys
+sys.path.insert(0,os.path.join(os.path.expanduser("~"),'Projects'))
+import albatros_analysis.src.baseband_data_classes as bdc
+
+ant=bdc.BasebandIterator()
+acclen=cut*1000
+pfb_size = acclen + 2*cut
+to_ipfb = cp.empty((pfb_size,nchans),dtype='complex64')
+
+# antennas = [ant1,ant2,...]
+# for i, chunks in enumerate(zip(*antennas)):
+    # for chunk in chunks:
+for i, chunk in enumerate(ant):
+    pol0=make_continuous(chunk.pol0)
+    if i==0:
+        raw_pol0 = ipfb(pol0) 
+    else:
+        to_ipfb[2*cut:] = pol0
+        assert to_ipfb.base is None
+        raw_pol0 = ipfb(to_ipfb)
+    pol0_new = pfb(raw_pol0[cut:-cut]) #size a bit smaller acclen - 2*cut for first chunk
+    to_ipfb[:2*cut] = pol0[-2*cut:] #store for next iteration
+
+    #do stuff with new pol0
