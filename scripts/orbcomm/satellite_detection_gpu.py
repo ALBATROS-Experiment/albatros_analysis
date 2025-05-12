1ee6001cfd06497c451ecb8c0c12e93e642e27db
Thomas Bolder <thomasb@nia1614.scinet.local>
2025-05-12 10:30:16 -0400

    modified sat det gpu, debug version of calibration script

--- scripts/orbcomm/satellite_detection_gpu.py
+++ scripts/orbcomm/satellite_detection_gpu.py
@@ -31,6 +31,7 @@ with open("config.json", "r") as f:
     coords = []
     # unpack information from the json file
     # Call get_starting_index for all antennas except reference
+    print('\n', "Antenna Details:")
     for i, (ant, details) in enumerate(config["antennas"].items()):
         # if ant != ref_ant:
         print(ant, details)
@@ -38,9 +39,20 @@ with open("config.json", "r") as f:
         dir_parents.append(details["path"])
     init_t = config["correlation"]["start_timestamp"]
     end_t = config["correlation"]["end_timestamp"]
-print(coords)
+    acclen = config["correlation"]["accumulation_length"]
 
-#Make a satmap
+print("Antenna Coordinates:", coords, '\n')
+print("Accumulation Length:", acclen, '\n')
+
+
+#----Define Reference Antenna----
+
+a1_coords = coords[0] 
+a1_path = dir_parents[0]
+
+
+
+#----Make a Satmap----
 
 #note that all the satellites are hard-coded in as of now. Need to find a way to generalize.
 # all the sats we track
@@ -57,77 +69,125 @@ for i, satnum in enumerate(satlist):
 
 
 
-# for each file get the risen sats and divide them up into unique transits
-a1_coords = coords[0] #ref ant]
-a1_path = dir_parents[0]
+
+#----Obtain and Plot the Risen Satellites----
+
+#Risen satellites found for three hours
+#only need the initial time since the TLE files go on for a while I believe.
+#do we need to do this only once or multiple times?
+#is three hours chosen arbitrarily? Something to do with validity of the TLE files?
 
 sat_data = {}
-
-nrows=int((3*3600)/T_ACCLEN) # for getting risen sats
+nrows=int((3*3600)/T_ACCLEN)
 tstart = init_t
 sat_data[tstart] = []
 tle_path = outils.get_tle_file(tstart, "/project/s/sievers/mohanagr/OCOMM_TLES")
-print("USING TLE PATH", tle_path)
+print("Using TLE path:", tle_path, '\n')
 arr = np.zeros((nrows, len(satlist)), dtype="int64")
+
+#list of lists of coordinates and ID of satellites that you can see at that time
 rsats = outils.get_risen_sats(tle_path, a1_coords, tstart, dt=5, niter=nrows,good=satlist,altitude_cutoff=15)
+#list of integers with how many are risen at that point
 num_sats_risen = [len(x) for x in rsats]
+
+#plotting
 fig, ax = plt.subplots(1, 2)
 fig.set_size_inches(10,4)
 fig.suptitle(f"Risen sats for file {tstart}")
 ax[0].plot(num_sats_risen)
 ax[0].set_xlabel("time (in units of 5 sec)")
 for i, row in enumerate(rsats):
-    for satnum, satele,sataz in row:
+    for satnum, satele, sataz in row:
         arr[i,satmap[satnum]] = 1
 ax[1].set_ylabel("time in units of 5 sec")
-ax[1].set_xlabel("Sat ID")
+ax[1].set_xlabel("Sat ID") #Sat ID with respect to the satmap dictionary index
 ax[1].imshow(arr,aspect='auto',interpolation="none")
 plt.tight_layout()
-fig.savefig(path.join(out_path,f"risen_sats_{tstart}_{str(time.time())}.jpg"))
+fig.savefig(path.join(out_path,f"risen_sats_{tstart}_{str(time.time())}_mark.jpg"))
+
+
+
+#----Get Pulses, AKA Satellite Transits----
+
+#pulses is in the form of [[start, end], [sats]]
+
 pulses = outils.get_simul_pulses(arr)
-print("Sat transits detected are:", pulses)
+print("Sat transits detected are:", pulses, '\n')
+print("Number of Pulses:", len(pulses), '\n')
 
 fig, ax = plt.subplots(np.ceil(len(pulses)/2).astype(int), 2)
 fig.set_size_inches(10, np.ceil(len(pulses)/2)*4)
 fig.suptitle(str(tstart))
 ax=ax.flatten()
 
+
+
+#----Antenna----
+#parse through each antenna 
+
 for antnum in range(1,len(dir_parents)):
+    #first we set up the coordinates and paths for the data from that specific antenna
+    #looks sketchy but not just for antenna 2, I think this is just the name for each antenna that is not the ref.
     print(f"--------------- ANTENNA {antnum}-----------------")
     a2_path = dir_parents[antnum]
     a2_coords = coords[antnum]
+
+    #this goes through each pulse (which for sanity check are indeed measured by all antenna, hence the shared 'pulses' list)
+    #pulses is based on 'arr', which is three hours long.
     for pnum, [(pstart, pend), sats] in enumerate(pulses):
-        print("running", pnum, pstart, pend, sats)
+        #we start by setting up all our required variables, and printing stuff too
+
+        print(f"------Pulse Number {pnum}-------")
+        print("Pulse Start Idx:", pstart)
+        print("Pulse End Idx:", pend)
+        print("Satelite Idxs Present:", sats, '\n')
+
+        #create new dictionary to store pulse information throughout this loop
         pulse_data = {}
         pulse_data["start"] = pstart
         pulse_data["end"] = pend
         pulse_data["sats"] = {}
         numsats_in_pulse = len(sats)
+        #define our pulse length
         t1 = tstart + pstart * T_ACCLEN
-        t2 = tstart + pend * T_ACCLEN  #probably do something about this. 
-        # dont need files for the entire pulse. some 50 sec chunk within the pulse will work
-        # t2 = t1 + 50
-        print("t1 t2 for the pulse are:", t1, t2)
+        t2 = tstart + pend * T_ACCLEN
+
+        #this is buggy and sets off the exception below. fix so that max 50 seconds of pulse read.
+        #if (pend-pstart)*T_ACCLEN > 50:
+        #    t2 = t1 + 50
+        #else:
+        #    t2 = tstart + pend * T_ACCLEN
+        #print("Full time difference:", tstart + pend * T_ACCLEN - t1)
+        #print("Maxed time difference:", t2-t1)
+
+        print("In ctime we measure Pulse t1:", t1, "Pulse t2:", t2)
+        print("Length of Pulse is:", t2-t1, '\n')
+
         try:
             files_a1, idx1 = butils.get_init_info(t1, t2, a1_path)
             files_a2, idx2 = butils.get_init_info(t1, t2, a2_path)
         except Exception as e:
             print(e)
-            print(f"skipping pulse {pstart}:{pend} in {tstart} as some file discontinuity was encountered.")
+            print(f"skipping pulse {pstart} to {pend} in {tstart} as some file discontinuity was encountered.")
             continue
         # print(files_a1,files_a2)
+
         channels = np.asarray(bdc.get_header(files_a1[0])["channels"],dtype='int64')
         chanstart = np.where(channels == 1834)[0][0]
         chanend = np.where(channels == 1852)[0][0]
         nchans = chanend - chanstart
-        size = 1000000 #should come from config
+
+
+        #recall we defined acclen above with the config file unpacking
         # #dont impose any chunk num, continue iterating as long as a chunk with small enough missing fraction is found.
         # #have passed enough files to begin with. should not run out of files.
+        print("Setting Antenna as BFI Objects", '\n')
+
         ant1 = bdc.BasebandFileIterator(
             files_a1,
             0,
             idx1,
-            size,
+            acclen,
             None,
             chanstart=chanstart,
             chanend=chanend,
@@ -137,19 +197,23 @@ for antnum in range(1,len(dir_parents)):
             files_a2,
             0,
             idx2,
-            size,
+            acclen,
             None,
             chanstart=chanstart,
             chanend=chanend,
             type="float",
         )
-        p0_a1 = cp.zeros((size, nchans), dtype="complex64") #remember that BDC returns complex64. wanna do phase-centering in 128.
-        p0_a2 = cp.zeros((size, nchans), dtype="complex64")
-        p0_a2_delayed = cp.zeros((size, nchans), dtype="complex64")
+
+        #set up our polarization arrays (for one chunk from the unpacked data?)
+
+        p0_a1 = cp.zeros((acclen, nchans), dtype="complex64") #remember that BDC returns complex64. wanna do phase-centering in 128.
+        p0_a2 = cp.zeros((acclen, nchans), dtype="complex64")
+        p0_a2_delayed = cp.zeros((acclen, nchans), dtype="complex64")
         niter = int(t2 - t1) + 1  # run it for an extra second to avoid edge effects
         print("niter for delay is", niter, "t1 is", t1)
-        delays = np.zeros((size, len(sats)))
-        # get geo delay for each satellite from Skyfield
+
+        delays = np.zeros((acclen, len(sats)))
+        # get geo delay for each satellite present in the pulse from Skyfield
         for i, satID in enumerate(sats):
             d = outils.get_sat_delay(
                 a1_coords,
@@ -160,18 +224,20 @@ for antnum in range(1,len(dir_parents)):
                 satmap[satID],
             )
             delays[:, i] = np.interp(
-                np.arange(0, size) * T_SPECTRA, np.arange(0, niter), d
+                np.arange(0, acclen) * T_SPECTRA, np.arange(0, niter), d
             )
             # print(f"delay for {satmap[satID]}", delays[0:10,i], delays[-10:,i])
-        # get baseband chunk for the duration of required transit. Take the first chunk `size` long that satisfies missing packet requirement
+        # get baseband chunk for the duration of required transit. Take the first chunk `acclen` long that satisfies missing packet requirement
         # print(delays[0:10,1], delays[-10:,1])
         # print(delays[0:10,0], delays[-10:,0])
         delays = cp.asarray(delays)
         a1_start = ant1.spec_num_start
         a2_start = ant2.spec_num_start
+
+        #iterates so that we take the first chunk which is above tolenance fill (I THINK?)
         for i, (chunk1, chunk2) in enumerate(zip(ant1, ant2)):
-            perc_missing_a1 = (1 - len(chunk1["specnums"]) / size) * 100
-            perc_missing_a2 = (1 - len(chunk2["specnums"]) / size) * 100
+            perc_missing_a1 = (1 - len(chunk1["specnums"]) / acclen) * 100
+            perc_missing_a2 = (1 - len(chunk2["specnums"]) / acclen) * 100
             print("missing a1", perc_missing_a1, "missing a2", perc_missing_a2)
             if perc_missing_a1 > 10 or perc_missing_a2 > 10:
                 a1_start = ant1.spec_num_start
@@ -181,23 +247,31 @@ for antnum in range(1,len(dir_parents)):
             # print(chunk2["pol0"])
             # print("nchans is", nchans)
             # print("first row chunk1", chunk1['pol0'][0,:])
-            bdc.make_continuous_gpu(chunk1['pol0'],chunk1['specnums']-a1_start,np.arange(nchans),size,nchans=nchans, out=p0_a1)
-            bdc.make_continuous_gpu(chunk2['pol0'],chunk2['specnums']-a2_start,np.arange(nchans),size,nchans=nchans, out=p0_a2)
+            bdc.make_continuous_gpu(chunk1['pol0'],chunk1['specnums']-a1_start,np.arange(nchans),acclen,nchans=nchans, out=p0_a1)
+            bdc.make_continuous_gpu(chunk2['pol0'],chunk2['specnums']-a2_start,np.arange(nchans),acclen,nchans=nchans, out=p0_a2)
             # print("first row p0_a1", p0_a1[0,:])
             break
+
         #If we detect a sat, note down what file we started from and what the specnum at the start was. 
-        info_tstamps_a1 = str(butils.get_tstamp_from_filename(files_a1[0]))+":"+str(ant1.spec_num_start-size)
-        info_tstamps_a2 = str(butils.get_tstamp_from_filename(files_a2[0]))+":"+str(ant2.spec_num_start-size)
+        info_tstamps_a1 = str(butils.get_tstamp_from_filename(files_a1[0]))+":"+str(ant1.spec_num_start-acclen)
+        info_tstamps_a2 = str(butils.get_tstamp_from_filename(files_a2[0]))+":"+str(ant2.spec_num_start-acclen)
         specnum_offset = ant1.spec_num_start - ant2.spec_num_start #this is the initial delay between specnums when the antennas booted up
         print(info_tstamps_a1)
         print(info_tstamps_a2)
         print("initial specnum offset 1 - 2", specnum_offset)
+
+        # do coarse xcorr to align the spectrum numbers
+
         cx = []  # store coarse xcorr for each satellite
-        N = 2 * size
+        N = 2 * acclen
         dN = min(100000, int(0.3 * N))
         print("2*N and 2*dN", N, dN)
         temp_satmap = []  # will need to map the row number to satID later
         cx.append(outils_g.coarse_xcorr(p0_a1, p0_a2, dN))  # no correction
+
+
+        #debug option which makes a plot of all channels and the coarse xcorrs
+        #helps visually see the peaks
         if DEBUG:
             fig2, ax2 = plt.subplots(np.ceil(cx[0].shape[0]/3).astype(int), 3)
             fig2.set_size_inches(12, np.ceil(cx[0].shape[0]/3)*3)
@@ -212,9 +286,14 @@ for antnum in range(1,len(dir_parents)):
             fig2.savefig(path.join(out_path,f"debug_cxcorr_{tstart}_{pstart}_{pend}.jpg"))
             print(path.join(out_path,f"debug_cxcorr_{tstart}_{pstart}_{pend}.jpg"))
             # sys.exit(0)
+
+        
         temp_satmap.append("default")  # zeroth row is always "no phase"
         # get beamformed visibilities for each satellite
         freqs = 250e6 * (1 - cp.arange(1834, 1852) / 4096)
+
+
+        #goes through each satellite present in the pulse
 
         for i, satID in enumerate(sats):
             print("processing satellite:", satmap[satID])
