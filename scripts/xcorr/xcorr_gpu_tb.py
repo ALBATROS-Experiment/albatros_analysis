5176e08039d28c2fe2a2691057e8cfed41de56b9
Thomas Bolder <thomasb@nia1622.scinet.local>
2025-08-11 15:24:56 -0400

    gpu into pyuv changes

--- scripts/xcorr/xcorr_gpu_tb.py
+++ scripts/xcorr/xcorr_gpu_tb.py
@@ -8,13 +8,34 @@ import helper as hp
 import helper_gpu as hpg
 sys.path.insert(0,path.expanduser("~"))
 import json
+from astropy.coordinates import EarthLocation
+from astropy import units as u
+from astropy.time import Time
+from pyuvdata import UVData
 
-outdir = "/project/s/sievers/thomasb/may26/vis"
 
 def arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument("--config", type=str, default="config3.json", help="Path to config file")
+
+    parser.add_argument(
+        "-c", 
+        "--config", 
+        type=str, 
+        default="config3.json", 
+        help="Path to config file"
+        )
+
+    parser.add_argument(
+        "-o", 
+        '--outdir', 
+        type=str, 
+        default="/project/sievers/s/thomasb", 
+        help="Output file path"
+        )
+
     return parser.parse_args()
+
+
 
 def run_script(config_path):
     with open(config_path, "r") as f:
@@ -25,36 +46,107 @@ def run_script(config_path):
         config["antennas"].keys(),
         key=lambda ant: config["antennas"][ant]["offset"],
     )
+    ant_names = []
+    ant_coords = []
     dir_parents = []
     spec_offsets = []
-    # Call get_starting_index for all antennas except reference
     for i, (ant, details) in enumerate(config["antennas"].items()):
-        # if ant != ref_ant:
-        print(ref_ant, ant, details)
+        ant_names.append(details["name"])
         dir_parents.append(details["path"])
         spec_offsets.append(details["offset"])
+        LLAcoords = details["coordinates"]
+        location = EarthLocation(lat=LLAcoords[0]*u.deg, lon=LLAcoords[1]*u.deg, height=LLAcoords[2]*u.m)
+        ant_coords.append(location.itrs.cartesian.xyz.to(u.m).value)
+    nants = len(ant_names)
 
+    osamp = 64
     init_t = config["correlation"]["start_timestamp"]
     end_t = config["correlation"]["end_timestamp"]
     pfb_size = config["correlation"]["accumulation_length"]
+    #t_acclen = pfb_size*4096/250e6
     chanstart = config["frequency"]["start_channel"]
     chanend = config["frequency"]["end_channel"]
-
-    osamp = 64
     nchunks = int(np.floor((end_t-init_t)*250e6/4096/pfb_size/osamp))
     idxs, files = hp.get_init_info_all_ant(init_t, end_t, spec_offsets, dir_parents)
+
     print("total time (in s) of visibility computation", end_t - init_t)
     print("final idxs", idxs)
     print("PFB SIZE:", pfb_size)
     print("OVERSAMPLING", osamp)
-    # t_acclen = acclen*4096/250e6
-    # sys.exit()
+
     t1=time.time()
     pols,missing_fraction,channels=hpg.repfb_xcorr_avg(idxs,files,pfb_size,nchunks,chanstart,chanend,osamp,cutsize=16,filt_thresh=0.45)
     t2=time.time()
     print("Total time taken", t2-t1)
 
-    return pols, missing_fraction, channels
+    uv = UVData()
+    uv.Nants_telescope, uv.Nants_data = nants, nants
+    _, _, uv.Nfreqs, uv.Ntimes = pols.shape
+    uv.Nbls = ((nants) * (nants-1))/2
+    uv.Nblts = uv.Ntimes * uv.Nbls
+    uv.Nants_telescope = uv.Nants_data #assume all ants have data
+    uv.antenna_names = ant_names
+    uv.antenna_numbers = np.arange(uv.Nants_data, dtype=int)
+    uv.antenna_positions = np.array(ant_coords, dtype=float)
+    uv.telescope_location = uv.antenna_positions[0]
+    print('uv tel loc', uv.telescope_location)
+
+
+
+    #PRIMARY DATA
+    nt = uv.Ntimes
+    na = uv.Nants_data
+    visl = []
+    freql = []
+    a1l = []
+    a2l = []
+    codel = []
+    timel = []
+    nsl = []
+
+    #fix this to work for the GPU function
+    idx = 0
+    for i in range(na):
+        for j in range(i+1,na):
+            #data = pols.data[:,idx,:,:].transpose(0,2,1)
+            #flags = pols.mask[:,idx,:,:].transpose(0,2,1)
+            #nsamples = rowcounts[:,idx]
+            #v_l.append(data)
+            #f_l.append(flags)
+            #ns_l.append(nsamples)
+
+            #a1_l.extend([i] * nt)
+            #a2_l.extend([j] * nt)
+
+            #code = int(i*2048 + j + 2**(16))
+            #c_l.extend([code] * nt)
+
+            #unix_times = np.linspace(init_t, init_t + (uv.Ntimes - 1) * t_acclen, uv.Ntimes)
+            #times = Time(unix_times, format='unix', scale='utc')
+            #jtimes = times.jd
+            #t_l.append(jtimes) # we do stuff in julian times
+
+            #print((i,j), idx)
+            #idx += 1
+
+    uv.data_array = np.concatenate(visl)
+    uv.flag_array = np.concatenate(freql)
+    uv.ant_1_array = np.array(a1l)
+    uv.ant_2_array = np.array(a2l)
+    uv.baseline_array = np.array(codel)
+    uv.time_array = np.concatenate(timel)
+    uv.set_lsts_from_time_array() #automatically sets LST list from other info
+
+    #nsamples and integration time
+    nsample_array = np.concatenate(nsl) #BEWARE for nsamples: same for each channel if pol, bline kept constant.
+    uv.nsample_array = np.tile(nsample_array[:, np.newaxis, :], (1, uv.Nfreqs, 1))
+    uv.nsample_array = uv.nsample_array.astype(float)
+    uv.integration_time = np.full(uv.Nblts, t_acclen)
+
+    print("saved data. shape:", uv.data_array.shape)
+
+
+
 
 
 
