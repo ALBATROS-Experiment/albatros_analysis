6f3be3b702dd9dcbb605c40bca97ad6b3b783bda
Mohan <mohan.agr97@gmail.com>
2025-08-29 15:55:59 -0400

    added some xcorr tests

--- src/utils/tests/test_streaming_xcorr.py
+++ src/utils/tests/test_streaming_xcorr.py
@@ -9,6 +9,19 @@ import numpy as np
 
 np.random.seed(42)
 
+def dumb_xcorr(x,nant, npol, nfreq):
+    truth = cp.zeros((nant*npol,nant*npol, nfreq),dtype='complex64',order='F')
+    for freq in range(nfreq):
+        for antidx1 in range(nant):
+            for polidx1 in range(npol):
+                for antidx2 in range(nant):
+                    for polidx2 in range(npol):
+                        # print(antidx1,polidx1,antidx2,polidx2)
+                        idx1 = antidx1*npol + polidx1
+                        idx2 = antidx2*npol + polidx2
+                        truth[idx1 ,idx2, freq] = cp.mean(x[idx1,:,freq] * np.conj(x[idx2,:,freq]))
+    return truth
+
 def test_xcorr_simple():
     niter = 10
     specsize = 11
@@ -40,6 +53,46 @@ def test_xcorr_simple():
         print(chunks[0][:,:,1])
         print(chunks[0][:,:,2])
         print(chunks[0][:,:,3])
+
+def test_xcorr():
+    acclen = 10
+    specsize = 11 #will pass to xcorr func in chunks of specsize
+    acclen_big =  specsize * acclen * 5
+    niter = acclen_big//specsize
+    nrows = acclen_big//acclen
+    nant = 8
+    npol = 2
+    nchan = 4
+    out = cp.empty((nant*npol,nant*npol, nchan, nrows),dtype='complex64',order='F')
+    truth = cp.empty((nant*npol,nant*npol, nchan, nrows),dtype='complex64',order='F')
+    data = {}
+    x = cp.empty((nant*npol,acclen_big,nchan),dtype='complex64',order='F') #full input data
+    sc = pu.StreamingCorrelator(nant, npol, acclen, np.arange(nchan))
+    for i in range(nant):
+        temp = {}
+        for j in range(npol):
+            temp[j] = (cp.random.randn(acclen_big*nchan).reshape(-1,nchan) + 1j*cp.random.randn(acclen_big*nchan).reshape(-1,nchan)).astype('complex64')
+            x[i*npol+j, :,:] = temp[j]
+        data[i]=temp
+
+    print("generating truth...")
+    for i in range(nrows):
+        truth[:,:,:,i] = dumb_xcorr(x[:,i*acclen:(i+1)*acclen,:],nant,npol,nchan)
+    print("running streaming xcorr...")
+    idx=0
+    for i in range(niter):
+        print("---iteration----", i)
+        for antidx in range(nant):
+            for polidx in range(npol):
+                spectra = data[antidx][polidx] #fioll
+                sc.load(antidx, polidx, spectra[i * specsize : (i + 1) * specsize])
+        chunks = sc.xcorr()
+        n = len(chunks)
+        if n > 0:
+            for ch in chunks:
+                out[:,:,:,idx] = ch
+                idx+=1
+    assert cp.allclose(out,truth,atol=1e-5,rtol=1e-6)
 
 
 if __name__ == "__main__":
