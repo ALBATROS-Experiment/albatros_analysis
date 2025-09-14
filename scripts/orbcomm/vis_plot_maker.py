cc9922680d285c2de88a00cfce355a78bf5c6c29
Thomas Bolder <thomasbolder39@gmail.com>
2025-09-13 23:47:05 -0400

    worked on vis plot to make better usability

--- scripts/orbcomm/vis_plot_maker.py
+++ scripts/orbcomm/vis_plot_maker.py
@@ -121,38 +121,70 @@ print(antennas)
 
 #-------------get some visibilities (all baselines)---------------
 
-#manually pick what pulse I want to look at
-desired_pulse_idx = 4
+
+
+# hard coded pulse start time and baselines
+desired_pulse_start_time = 6340   #this is the current (not super thought out) way to identify pulses
+bl1_ants = set({'Antenna 1', 'Antenna 4'})
+bl2_ants = set({'Antenna 4', 'Antenna 8'})
+required_antennas = bl1_ants | bl2_ants  #in order to later maybe keep track to reduce unnecessary computation
+
+
+#here I want to extract the pulse information
+#the logic here is not great. if both are non-refs there will be a lot to verify
+pd1, pd2 = None, None
+for i, details in enumerate(antennas):
+    if details['name'] == 'Antenna 1':
+        continue
+    elif details['name'] in bl1_ants:
+        for pulse in details['pulse_data']:
+            if pulse['start'] == desired_pulse_start_time:
+                pd1 = pulse
+    elif details['name'] in bl2_ants:
+        for pulse in details['pulse_data']:
+            if pulse['start'] == desired_pulse_start_time:
+                pd2 = pulse
+
+if pd1 == None:
+    raise ValueError(f"no pulse for start time {desired_pulse_start_time} for bline 1")
+if pd2 == None:
+    raise ValueError(f"no pulse for start time {desired_pulse_start_time} for bline 2")
+
+
+#extract antenna information (in list to preserve order)
 nant = len(antennas)
 ant_offsets = []
 ant_paths = []
 ant_names = []
-
-#extract information
 for i, ant in enumerate(antennas):
     ant_offsets.append(ant['consensus_offset'])
     ant_paths.append(ant['path'])
     ant_names.append(ant['name'])
 
-#set up bline map, find desired baselines
 
+#set up bline map, find desired baselines in terms of their index
 bl_map = []
 for i in range(nant):
     for j in range(i+1, nant):
-        bl_map.append(f'{ant_names[i]}_{ant_names[j]}')
+        bl_elements = set({ant_names[i], ant_names[j]})
+        bl_map.append(bl_elements)
 print('baseline map:', bl_map)
 
-desired_bl_1 = 'Antenna 1_Antenna 2'
-desired_bl_2 = 'Antenna 2_Antenna 7'
-bl1 = [i for i, x in enumerate(bl_map) if x == desired_bl_1]
-bl2 = [i for i, x in enumerate(bl_map) if x == desired_bl_2]
+bl1 = [i for i, x in enumerate(bl_map) if x == bl1_ants]
+bl2 = [i for i, x in enumerate(bl_map) if x == bl2_ants]
 print('bl1, bl2:', bl1, bl2)
 
 #set up some times, channels, and compute the visibilities for all blines
 #note that we are taking times and such wrt Ant1-Ant2 bline. 
 #this is fine since the pulse times are all the same for each bline
-desired_pulse_info = antennas[1]['pulse_data'][desired_pulse_idx]
-rel_start_t, rel_end_t = desired_pulse_info['start'], desired_pulse_info['end']
+
+pulse_end_t1, pulse_end_t2 = pd1['end'], pd2['end']
+if pulse_end_t1 != pulse_end_t2:
+    raise ValueError('pulse time data does not agree')
+
+#add some more double check here or something.
+
+rel_start_t, rel_end_t = pd1['start'], pd1['end']
 pulse_start_t = rel_start_t + global_start_time
 pulse_end_t = rel_end_t + global_start_time
 pulse_len_chunks = int(np.ceil((pulse_end_t - pulse_start_t)/(T_SPECTRA * v_acclen)))
@@ -167,6 +199,7 @@ channels = bdc.get_header(files[0][0])["channels"].astype('int64')
 chanstart = np.where(channels == 1834)[0][0] 
 chanend = np.where(channels == 1852)[0][0]
 print('starting, ending channels:', chanstart, chanend)
+chanlist = np.arange(1834, 1852)
 
 print('GETTING VISIBILITIES------------------')
 vis, rowcount, obj = helper.get_avg_fast2(idxs,files,v_acclen,pulse_len_chunks, chanstart, chanend)
@@ -177,15 +210,36 @@ vis, rowcount, obj = helper.get_avg_fast2(idxs,files,v_acclen,pulse_len_chunks, 
 #just have to pick what pol we want to work on.
 
 print('total vis shape:', vis.shape)
+
+#now we have some visibility data, so let's massage it a bit
 vis_1, vis_2 = vis[: , bl1, 0, :], vis[: , bl2, 0, :]  #may need to interpolate
 vis_2 = vis[: , bl2, 0, :]
 print('vis 1, vis 2 shapes:', vis_1.shape, vis_2.shape)
 vis_1, vis_2 = np.squeeze(vis_1), np.squeeze(vis_2)
 p_vis1, p_vis2 = np.angle(vis_1), np.angle(vis_2)
-phase1, phase2 = np.unwrap(p_vis1[:, 3]), np.unwrap(p_vis2[:, 3])  #automate the sat and channel selection
 
-chanlist = np.arange(1834, 1852)
+#here we want to auto-select which channel to use for phase display
+amp1, amp2 = np.abs(vis_1), np.abs(vis_2)
+mean_amp1, mean_amp2 = [], []
+for i in range(18):
+    mean_amp1.append(np.mean(amp1[:,i]))
+    mean_amp2.append(np.mean(amp2[:,i]))
 
+#have to play a little game with channel indices (s for small, b for big)
+chan_s_idx1, chan_s_idx2 = np.where(mean_amp1 == np.max(mean_amp1))[0][0], np.where(mean_amp2 == np.max(mean_amp2))[0][0]
+if chan_s_idx1 != chan_s_idx2:
+    print('CAUTION!!!! Same pulse is detected at different frequencies at different baselines')
+chan_b_idx1, chan_b_idx2 = chanlist[chan_s_idx1], chanlist[chan_s_idx2]
+
+#finally we can unwrap this guy
+phase1, phase2 = np.unwrap(p_vis1[:, chan_s_idx1]), np.unwrap(p_vis2[:, chan_s_idx2])  #automate the sat and channel selection
+
+
+
+
+
+
+#and now let's plot!
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
 ax[0,0].set_ylabel("Chunk Number (~0.5s interval)")
 ax[0,0].set_title("Wrapped Phase, All Channels")
@@ -193,7 +247,7 @@ im1 = ax[0,0].imshow(p_vis1, aspect='auto', cmap='RdBu', interpolation="none")
 fig.colorbar(im1)
 
 ax[0,1].set_ylabel("Phase (Radians)")
-ax[0,1].set_title("Unwrapped Phase, Detection Channel")
+ax[0,1].set_title(f"Unwrapped Phase, {chan_b_idx1}")
 ax[0,1].plot(phase1)
 #ax[0,1].plot(pred_phase_1)
 
@@ -207,4 +261,4 @@ ax[1,1].set_ylabel("Phase (Radians)")
 ax[1,1].plot(phase2)
 #ax[0,1].plot(pred_phase)
 
-fig.savefig('/scratch/thomasb' + f'/2bline_pulse{desired_pulse_idx}_{global_start_time}.jpg')
+fig.savefig('/scratch/thomasb' + f'/2bline_pulse{desired_pulse_start_time}_{global_start_time}.jpg')
