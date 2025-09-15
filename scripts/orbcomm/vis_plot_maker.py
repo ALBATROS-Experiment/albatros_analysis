f93bde3584454b1f74eaacba97f56174f9d71d97
Thomas Bolder <thomasbolder39@gmail.com>
2025-09-15 16:34:03 -0400

    rudimentary file simultaneity checker with plotting.

--- scripts/orbcomm/vis_plot_maker.py
+++ scripts/orbcomm/vis_plot_maker.py
@@ -12,7 +12,6 @@ from datetime import datetime as dt
 from src.correlations import baseband_data_classes as bdc
 from src.utils import baseband_utils as butils
 from src.utils import orbcomm_utils as outils
-from scripts.xcorr import helper
 from scipy.optimize import least_squares
 import json
 #import extra_functions as ef
@@ -21,23 +20,6 @@ import random
 from scripts.xcorr import helper as hp
 import importlib
 from scipy.interpolate import interp1d
-
-
-#---------------define some functions----------------
-
-#CHECK TO SEE IF THIS MAKES SENSE/IS ALLOWED
-def interp_rect(vis):
-
-    #interpolates rectangular form
-    t = np.arange(len(vis))
-    valid = ~vis.mask
-
-    real_interp = interp1d(t[valid], vis[valid].real, kind='linear', fill_value="extrapolate")
-    imag_interp = interp1d(t[valid], vis[valid].imag, kind='linear', fill_value="extrapolate")
-    
-    vis_interp = real_interp(t) + 1j * imag_interp(t) 
-    return vis_interp
-
 
 def pred(coord1, coord2, start_t, end_t, pulse_idx, channel, satID):
     '''
@@ -64,22 +46,28 @@ def pred(coord1, coord2, start_t, end_t, pulse_idx, channel, satID):
     return pred  
 
 
-#--------------------set up some paths------------
 
+
+#--------------------hard coded setup------------
 config_path = '/home/thomasb/albatros_analysis/scripts/orbcomm'
 config_name = 'config.json'
 
-satdet_path = '/scratch/thomasb'
+satdet_path = '/scratch/thomasb/pulsedata_1753133403'
 satdet_name = "pulsedata_1753133403_1757540615.4208999.json"
 
+T_SPECTRA = 4096/250e6
 
-#-------------------unpack some information----------
+desired_pulse_start_time = 6340   #this is the current (not super thought out) way to identify pulses
+bl1_ants = set({'Antenna 1', 'Antenna 7'})
+bl2_ants = set({'Antenna 1', 'Antenna 8'})
+required_antennas = bl1_ants | bl2_ants
+
+
+#-------------------unpack information---------------
 
 antennas = []
-
 with open(f"{config_path}/{config_name}", "r") as f:
     config = json.load(f)
-
     for i, (ant, details) in enumerate(config["antennas"].items()):
         ant_dict = {}
         ant_dict['name'] = details['name']
@@ -91,68 +79,54 @@ with open(f"{config_path}/{config_name}", "r") as f:
         else:
             ant_dict['reference'] = 'F'
 
-        antennas.append(ant_dict)
+        if ant_dict['name'] in required_antennas:
+            antennas.append(ant_dict)
 
     global_start_time = config["correlation"]["start_timestamp"]
     global_end_time = config["correlation"]["end_timestamp"]
     v_acclen = config["correlation"]["vis_acclen"]
-    T_SPECTRA = 4096/250e6
+    
+nants = len(antennas)
 print(antennas)
+print('number of antenna:', nants)
 print("Visibility Accumulation Length", v_acclen)
-print(global_start_time)
-print(global_end_time)
-print(T_SPECTRA)
+print('global start and end times:', global_start_time, global_end_time)
 
-V_T_ACCLEN = v_acclen* T_SPECTRA
 tle_path = outils.get_tle_file(global_start_time, "/project/rrg-sievers/mohanagr/OCOMM_TLES")
+chunk_length = T_SPECTRA * v_acclen
 
-print(len(antennas))
-
-
-antennas[0]['consensus_offset'] = 0
 with open(f'{satdet_path}/{satdet_name}') as f:
-    pulsedata = json.load(f)
-    ants = pulsedata[f'{global_start_time}']
-    for i, (ant, details) in enumerate(ants.items()):
-        antennas[i+1]['consensus_offset'] = details['consensus_offset']
-        antennas[i+1]['pulse_data'] = details['pulse_data']
+    data = json.load(f)
+    ants = data[f'{global_start_time}']
+    for i, ant_from_list in enumerate(antennas):
+        if ant_from_list['name'] == 'Antenna 1':
+            antennas[i]['consensus_offset'] = 0
+            continue
+        for j, (ant_from_satdet, details) in enumerate(ants.items()):
+            if ant_from_list['name'] == ant_from_satdet:
+                antennas[i]['consensus_offset'] = details['consensus_offset']
+                antennas[i]['pulse_data'] = details['pulse_data']
 
-print(antennas)
+for term in antennas:
+    print(term)
 
-#-------------get some visibilities (all baselines)---------------
+#---------extract pulse information---------
 
-
-
-# hard coded pulse start time and baselines
-desired_pulse_start_time = 6340   #this is the current (not super thought out) way to identify pulses
-bl1_ants = set({'Antenna 1', 'Antenna 4'})
-bl2_ants = set({'Antenna 4', 'Antenna 8'})
-required_antennas = bl1_ants | bl2_ants  #in order to later maybe keep track to reduce unnecessary computation
-
-
-#here I want to extract the pulse information
-#the logic here is not great. if both are non-refs there will be a lot to verify
-pd1, pd2 = None, None
+desired_pulse_info_all = []
 for i, details in enumerate(antennas):
     if details['name'] == 'Antenna 1':
         continue
-    elif details['name'] in bl1_ants:
-        for pulse in details['pulse_data']:
-            if pulse['start'] == desired_pulse_start_time:
-                pd1 = pulse
-    elif details['name'] in bl2_ants:
-        for pulse in details['pulse_data']:
-            if pulse['start'] == desired_pulse_start_time:
-                pd2 = pulse
+    pulse_present = False
+    for pulse in details['pulse_data']:
+        if pulse['start'] == desired_pulse_start_time:
+            desired_pulse_info_all.append(pulse)
+            pulse_present = True
 
-if pd1 == None:
-    raise ValueError(f"no pulse for start time {desired_pulse_start_time} for bline 1")
-if pd2 == None:
-    raise ValueError(f"no pulse for start time {desired_pulse_start_time} for bline 2")
+if pulse_present == False:
+    print(f"CAUTION: not all antenna have pulse {desired_pulse_start_time}!!!!")
 
 
-#extract antenna information (in list to preserve order)
-nant = len(antennas)
+#----------extract antenna information---------
 ant_offsets = []
 ant_paths = []
 ant_names = []
@@ -161,11 +135,10 @@ for i, ant in enumerate(antennas):
     ant_paths.append(ant['path'])
     ant_names.append(ant['name'])
 
-
 #set up bline map, find desired baselines in terms of their index
 bl_map = []
-for i in range(nant):
-    for j in range(i+1, nant):
+for i in range(nants):
+    for j in range(i+1, nants):
         bl_elements = set({ant_names[i], ant_names[j]})
         bl_map.append(bl_elements)
 print('baseline map:', bl_map)
@@ -178,22 +151,18 @@ print('bl1, bl2:', bl1, bl2)
 #note that we are taking times and such wrt Ant1-Ant2 bline. 
 #this is fine since the pulse times are all the same for each bline
 
-pulse_end_t1, pulse_end_t2 = pd1['end'], pd2['end']
-if pulse_end_t1 != pulse_end_t2:
-    raise ValueError('pulse time data does not agree')
-
 #add some more double check here or something.
 
-rel_start_t, rel_end_t = pd1['start'], pd1['end']
+rel_start_t, rel_end_t = desired_pulse_info_all[0]['start'], desired_pulse_info_all[0]['end']
 pulse_start_t = rel_start_t + global_start_time
 pulse_end_t = rel_end_t + global_start_time
-pulse_len_chunks = int(np.ceil((pulse_end_t - pulse_start_t)/(T_SPECTRA * v_acclen)))
+pulse_len_chunks = int(np.ceil((pulse_end_t - pulse_start_t)/chunk_length))
 
 print('relative start, end t:', rel_start_t, rel_end_t)
 print('pulse_duration:', rel_end_t-rel_start_t)
 
 print('GETTING INIT INFO --------------------')
-idxs, files = helper.get_init_info_all_ant(pulse_start_t, pulse_end_t, ant_offsets, ant_paths)
+idxs, files = hp.get_init_info_all_ant(pulse_start_t, pulse_end_t, ant_offsets, ant_paths)
 
 channels = bdc.get_header(files[0][0])["channels"].astype('int64')
 chanstart = np.where(channels == 1834)[0][0] 
@@ -202,7 +171,7 @@ print('starting, ending channels:', chanstart, chanend)
 chanlist = np.arange(1834, 1852)
 
 print('GETTING VISIBILITIES------------------')
-vis, rowcount, obj = helper.get_avg_fast2(idxs,files,v_acclen,pulse_len_chunks, chanstart, chanend)
+vis, rowcount, obj = hp.get_avg_fast2(idxs,files,v_acclen,pulse_len_chunks, chanstart, chanend)
 
 #vis is shape (nchunks, nbl, npols, ncols)
 #where nbl index is our baseline_map index
@@ -237,9 +206,8 @@ phase1, phase2 = np.unwrap(p_vis1[:, chan_s_idx1]), np.unwrap(p_vis2[:, chan_s_i
 
 
 
+#--------------------------plotting--------------------------------
 
-
-#and now let's plot!
 fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(10, 8))
 ax[0,0].set_ylabel("Chunk Number (~0.5s interval)")
 ax[0,0].set_title("Wrapped Phase, All Channels")
