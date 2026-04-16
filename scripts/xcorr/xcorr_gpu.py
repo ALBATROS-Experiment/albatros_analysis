5be27d6926d60b27bdacba1e3bdf9ae3c3915481
Mohan <mohan.agr97@gmail.com>
2026-04-16 19:34:17 -0400

    disk read/write async optimization

--- scripts/xcorr/xcorr_gpu.py
+++ scripts/xcorr/xcorr_gpu.py
@@ -63,7 +63,7 @@ if __name__=="__main__":
     nant = len(dir_parents)
     npol = 2
     nrows_total = nchunks * pfb_size // (osamp * new_acclen)
-    tag = 'summer25_fullday_band2'
+    tag = 'with_changes_IQ'
     timestamp = datetime.datetime.now().strftime("%Y%m%dT%H%M%S")
     # uid = str(uuid.uuid4())[:4]  # short unique suffix
     bit_mode = 1
